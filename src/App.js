8b89384e293fc152f5766183eafa272157daf525
ruijuanwang <1938608827@qq.com>
2020-04-27 18:52:25 +0800

    Home组件-配置4个子路由

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route,Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route,Link, Redirect } from 'react-router-dom'
 import Home from './pages/Home' // 导入Home组件
 import Citylist from './pages/Citylist' // 导入Citylist组件
  
@@ -8,7 +8,16 @@ class App extends React.Component{
     // BrowserRouter 必须在最外层包裹一次
     return <Router>
     <div>
-      我是APP根组件
+      {/* 我是APP根组件 */}
+      {/* 配置 / 默认地址 重定向 默认打开显示Index首页 */}
+      <Route
+      exact
+      path='/'
+      render={(props)=>{ // props参数 组件的 我们经常用的props 在这里没什么用
+          // 重定向 重新跳转到首页 / 默认跳转首页
+          return <Redirect to='/home/index'></Redirect>
+        }}
+      ></Route>
       {/* /home 是父路由组件 里面嵌套四个子路由 */}
       <Route path='/home' component={ Home }></Route>
       {/* 城市选择也是一个单独的页面 属于兄弟路由 同级写 */}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,19 @@
 import React,{ Component } from 'react'
 import { Route } from 'react-router-dom'
+import Index from '../Index' // 主页组件
+import Houselist from '../Houselist' // 找房组件
+import News from '../News' // 资讯组件
+import Profile from '../Profile' // 我的组件
 
 export default class Home extends Component{
     render(){
         return <div>
             我是home组件
-         
+            {/* Home组件有四个子路由 挖坑 index首页 houselist找房 news资讯 profile我的 */}
+            <Route path='/home/index' component={ Index } ></Route>
+            <Route path='/home/houselist' component={ Houselist } ></Route>
+            <Route path='/home/news' component={ News } ></Route>
+            <Route path='/home/profile' component={ Profile } ></Route>
             </div>
     }
 
