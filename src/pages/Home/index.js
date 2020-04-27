20d07b34ea37de1bf1f5b6db6c4807e172ab1b5a
ruijuanwang <1938608827@qq.com>
2020-04-27 21:16:48 +0800

    Home组件-循环的TabBar.Item封装为函数

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -38,30 +38,10 @@ export default class Home extends Component{
       selectedTab: '/home/index', // 用来控制 底部tab栏 选中高亮的 默认选中首页
       hidden: false
     };
-    // 函数
-
-    render(){
-        return <div className='home'>
-            {/* 我是home组件 */}
-            {/* Home组件有四个子路由 挖坑 index首页 houselist找房 news资讯 profile我的 */}
-            <Route path='/home/index' component={ Index } ></Route>
-            <Route path='/home/houselist' component={ Houselist } ></Route>
-            <Route path='/home/news' component={ News } ></Route>
-            <Route path='/home/profile' component={ Profile } ></Route>
-            {/* 在下面放底部tabbar标签栏组件 点击切换显示不同的路由组件页面 */}
-            {/* 使用autd-mobile里面的标签栏组件 TabBar*/}
-            {/* TabBar 是最外层的盒子 */}
-            <TabBar
-                unselectedTintColor="#bbb" // 未选中的文字颜色 
-                tintColor="#21b97a" // 选中的文字颜色
-                barTintColor="white" // 底部外层TabBar背景色
-                hidden={this.state.hidden} // 是否隐藏TabBar标签 true 隐藏  false 不隐藏
-                noRenderContent={ true } // 不渲染内容 true不渲染   false渲染  
-             >
-                 {/* TabBar.Item 每一项 */}
-                 {/* 底部栏 一共有四项 除了path地址(包括选中高亮)、icon字体图标、title标题不一样，其他都一样 所以删掉三个 只留一个 用来循环 */}
-                {/* 使用map循环 TabBar.Item 每一项  把tabItems这个数组循环*/}
-                { tabItems.map((item,index)=>{
+    // 循环渲染的四个tabbarItem 封装为一个函数 
+    renderItem(){
+    // 函数要有返回值
+     return  tabItems.map((item,index)=>{
                     // map函数要return
                     return <TabBar.Item
                         title={item.title} // 标题
@@ -87,7 +67,34 @@ export default class Home extends Component{
                         }}
                     >
                     </TabBar.Item>
-                }) }
+                }) 
+    }
+
+    render(){
+        return <div className='home'>
+            {/* 我是home组件 */}
+            {/* Home组件有四个子路由 挖坑 index首页 houselist找房 news资讯 profile我的 */}
+            <Route path='/home/index' component={ Index } ></Route>
+            <Route path='/home/houselist' component={ Houselist } ></Route>
+            <Route path='/home/news' component={ News } ></Route>
+            <Route path='/home/profile' component={ Profile } ></Route>
+            {/* 在下面放底部tabbar标签栏组件 点击切换显示不同的路由组件页面 */}
+            {/* 使用autd-mobile里面的标签栏组件 TabBar*/}
+            {/* TabBar 是最外层的盒子 */}
+            <TabBar
+                unselectedTintColor="#bbb" // 未选中的文字颜色 
+                tintColor="#21b97a" // 选中的文字颜色
+                barTintColor="white" // 底部外层TabBar背景色
+                hidden={this.state.hidden} // 是否隐藏TabBar标签 true 隐藏  false 不隐藏
+                noRenderContent={ true } // 不渲染内容 true不渲染   false渲染  
+             >
+                 {/* TabBar.Item 每一项 */}
+                 {/* 底部栏 一共有四项 除了path地址(包括选中高亮)、icon字体图标、title标题不一样，其他都一样 所以删掉三个 只留一个 用来循环 */}
+                {/* 使用map循环 TabBar.Item 每一项  把tabItems这个数组循环*/}
+
+                {/* react 经常把循环的大段代码 写在函数 调用 */}
+                {/* 调用底部item循环的函数 */}
+                { this.renderItem() }
    
          
             </TabBar>
