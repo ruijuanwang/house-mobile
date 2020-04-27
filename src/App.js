403dcb636c3e19f74d9feda39fa97ca1bd868218
ruijuanwang <1938608827@qq.com>
2020-04-27 11:55:58 +0800

    安装antd-mobile组件库并且导入以及导入默认样式

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from 'antd-mobile' // 导入组件 测试
 
 class App extends React.Component{
   render(){
     return <div>
       我是APP组件
+      <Button type="primary">primary</Button>
     </div>
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './utils/index.css' // 导入 默认样式
+import 'antd-mobile/dist/antd-mobile.css'; //引入 antd-mobile组件库的 默认css样式
 
 // 2.导入根组件
 import App from './App'; 
