3d63a318ae1a48015924e235d0a48824ebbb75af
ruijuanwang <1938608827@qq.com>
2020-04-27 11:40:59 +0800

    初始化

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
+// 1.导入 react 两个核心包
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './utils/index.css' // 导入 默认样式
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// 2.导入根组件
+import App from './App'; 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// 3.渲染页面
+ReactDOM.render(<App />,document.getElementById('root'));
+
