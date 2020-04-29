793cb3b58f96a0f51cbb3318fc6eac3bb42176fa
ruijuanwang <1938608827@qq.com>
2020-04-29 20:16:01 +0800

    首页-最新资讯-获取数据

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -37,17 +37,31 @@ export default class Home extends Component{
     swiperData: [], // 轮播图数据
     imgHeight: 176,
     isplay:false, // 控制轮播图是否自动播放 (应该会自动轮播，但是他不会自动播放，代码没错。原因就是 刚进来没优数据，自动轮播 没法判断怎么轮播 ，没有数据，所以我们应该等数据请求回来赋值到swiperData中才改为true，但是由于setState是异步的，所以要在第二个参数回调中，这时保证数据请求回来了，把isplsy自动播放改为true)
-    groups:[] // 接收租房小组的数据
+    groups:[], // 接收租房小组的数据
+    news:[] // 接收最新资讯的数据
   }
   componentDidMount() {
     // 应该发送请求  获取轮播图数据
     this.getSwiper() // 调用函数
     this.getGroups() // 调用函数 获取租房小组数据
+    this.getNews() // 调用函数 获取最新数据数据
   }
+  // 获取最新数据数据的函数
+  async getNews(){
+     // 发送请求
+     const res = await axios.get("http://api-haoke-dev.itheima.net/home/news?area=AREA%7C88cff55c-aaa4-e2e0")
+    //  console.log('最新资讯',res);
+     if(res.data.status===200){
+         this.setState({
+             news:res.data.body // 赋值到state中
+         })
+     }
+     
+    }
   // 租房小组 获取数据 调用接口
   async getGroups(){
-      const res = await axios('http://api-haoke-dev.itheima.net/home/groups?area=AREA%7C88cff55c-aaa4-e2e0')
-      console.log("租房小组数据",res);
+      const res = await axios.get('http://api-haoke-dev.itheima.net/home/groups?area=AREA%7C88cff55c-aaa4-e2e0')
+    //   console.log("租房小组数据",res);
      if(res.data.status===200){
         //  状态码为200就是成功
           this.setState({
@@ -157,14 +171,9 @@ export default class Home extends Component{
                           </Flex>
                       }}
                 />
-                    {/* <Grid >
-                        <Grid.Item>
-                            <div>家住回龙观</div>
-                            <img src={nav1} alt=''></img>
-                        </Grid.Item>
-                    </Grid>
-                 */}
-                </div>
+            </div>
+            {/* 4.最新资讯 */}
+
             </div>
     }
 
