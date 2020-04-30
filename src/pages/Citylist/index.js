23d87610fa27e6f05148a2500c96b18d54dd7e2d
ruijuanwang <1938608827@qq.com>
2020-04-30 22:39:12 +0800

    城市选择-获取城市数据以及格式化

--- src/pages/Citylist/index.js
+++ src/pages/Citylist/index.js
@@ -3,8 +3,51 @@ import React,{ Component } from 'react'
 import { NavBar, Icon } from 'antd-mobile'; 
 //  导入样式
 import './citylist.scss'
+import axios from 'axios'
 
 export default class Citylist extends Component{
+    componentDidMount(){
+        // 调用获取城市的方法
+        this.getCityList();
+    }
+    // 获取城市的数据的函数函数
+    async getCityList(){
+        // 调用接口 
+        let res = await axios.get('http://api-haoke-dev.itheima.net/area/city?level=1')
+        console.log('城市数据',res);
+        // 0: {label: "北京", value: "AREA|88cff55c-aaa4-e2e0", pinyin: "beijing", short: "bj"}
+        // 1: {label: "安庆", value: "AREA|b4e8be1a-2de2-e039", pinyin: "anqing", short: "aq"}
+        // 2: {label: "南宁", value: "AREA|2bc437ca-b3d2-5c3c", pinyin: "nanning", short: "nn"}
+        // 3: {label: "长沙", value: "AREA|98b03413-6f84-c263", pinyin: "changsha", short: "cs"}
+        // 获取来的数据 不是我们想要的格式 我们应该自己格式化：
+        // {
+        //   a:[a的城市1，a的城市2...],
+        //   b:[b的城市1，b的城市2...],
+        //     ...
+        // }
+        // 定义一个新的空对象 用来放城市列表
+        let citylist = {}
+        // 我们应该循环数组的每一项  改成我们想要的格式
+        res.data.body.forEach((item)=>{
+            // 每一个城市 根据拼音 bj 首字母b判断  从索引0开始截取一位
+             let word = item.short.substr(0,1) // 截取第一个单词
+            // 判断对象的键有没有第一个单词 有就push进去数组  没有就赋值一个数组
+            // 1.对象.属性 2.对象['属性']
+            // 对象中没有属性的话就会返回undefined 
+            // {b:[宝鸡，北海，北京]} 
+            // 如果对象里面没有  我们就赋值 为数组，如果有 我们就追加到数组
+            if(citylist[word]){
+                // 说明有这个key 我们应该把这一项 push进去
+                citylist[word].push(item)
+            }else{
+                // 说明没有数组，赋值给一个数组  并且把当前的item这一项加进去  否则就跳过了
+                citylist[word]=[item]
+            }
+        })
+        console.log('新的城市对象',citylist);
+        
+    }
+
     render(){
         return <div className='citylist'>
             {/* 我是Citylist组件 */}
