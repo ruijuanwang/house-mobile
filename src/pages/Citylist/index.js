3bfd289c3d6da8961cbb1d8e202112cb519bc02c
ruijuanwang <1938608827@qq.com>
2020-05-05 17:14:35 +0800

    导入使用顶部导航栏组件

--- src/pages/Citylist/index.js
+++ src/pages/Citylist/index.js
@@ -1,6 +1,8 @@
 import React,{ Component } from 'react'
 // 导入组件
-import { NavBar, Icon, Toast } from 'antd-mobile'; 
+import { Toast } from 'antd-mobile'; 
+import NavHeader from '../../components/NavHeader' // 导入导航栏组件
+
 //  导入样式
 import './citylist.scss'
 import axios from 'axios'
@@ -245,17 +247,8 @@ export default class Citylist extends Component{
     render(){
         return <div className='citylist'>
             {/* 我是Citylist组件 */}
-            {/* 1.顶部导航栏组件 NavBar */}
-             <NavBar
-             className='navbar'
-             mode="light" // 导航栏的颜色 dark 蓝色 light白色
-             icon={<Icon type="left" />} // 左侧图标 自带的
-             onLeftClick={() =>
-                //  点击左侧图标 触发事件 应该返回上一页
-                // this.props.history.goBack()
-                this.props.history.go(-1)
-             }
-            >城市选择</NavBar>
+            {/* 1.顶部导航栏组件 NavBar 使用自己封装的组件 */}
+           <NavHeader>城市选择</NavHeader>
 
             {/* 2.内容 List组件 可视区域渲染  */}
             {/* 可视区域的宽高度是动态渲染的 也就是除了导航栏剩下的就是我们要显示的区域
