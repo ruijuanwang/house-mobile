91f314fdfc84c484acaff6e4b389a901b7567d00
ruijuanwang <1938608827@qq.com>
2020-05-05 11:47:41 +0800

    封装顶部导航栏组件-点击返回报错-使用withRouter包裹组件

--- src/components/NavHeader/index.js
+++ src/components/NavHeader/index.js
@@ -1,13 +1,14 @@
 // NavHeader 顶部导航栏组件
 
 import React, { Component } from 'react'
+import {withRouter} from 'react-router-dom' // 路由
 
 // 导入组件
 import { NavBar, Icon } from 'antd-mobile';
 // 导入组件样式
 import './navheader.scss'
 // 封装 NavHeader 顶部导航栏
-export default class NavHeader extends Component {
+class NavHeader extends Component {
   render() {
     return <NavBar
             className="navbar"
@@ -26,3 +27,10 @@ export default class NavHeader extends Component {
           </NavBar>
   }
 }
+// 1.封装的组件导入使用 history路由就没有了
+// 正常路由打开的组件默认是有history路由操作的
+// 注意：如果自己单独封装了一个组件 在导入使用history就没有了
+// 解决方法 :当组件不能使用路由的时候 就用withRouter包裹 
+export default withRouter(NavHeader) 
+// withRouter(NavHeader) 就是react封装的高阶组件
+
