8e38479d08a50aaacf77ae3e63c6a4f77956fa32
ruijuanwang <1938608827@qq.com>
2020-05-05 17:46:45 +0800

    全局与局部样式CSS Modules

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,6 +1,8 @@
 import React,{ Component } from 'react'
 import './map.scss' // 引入样式
 import NavHeader from '../../components/NavHeader'
+// 导入局部样式   
+import styles from './map.module.css'
 
 let BMap = window.BMap // 使用地图函数要加 window. 来调用
 export default class Map extends Component{
@@ -18,7 +20,12 @@ export default class Map extends Component{
         map.centerAndZoom(point,15) // 数字越大 地图就会放大 看到的就更精确 放大缩小
     }
     render(){
+            // console.log(styles); // {news: "map_news__1mbE2"}=> styles是一个对象{news:react新取的唯一名字} 
         return <div className='map'>
+            
+            {/*局部样式 使用：className={styles.news} */}
+            {/*styles.news=> class="map_news__1mbE2" 其实就是帮我们取一个新的名字。 */}
+            {/* <div className={styles.news}>map啊啊啊</div> */}
                 {/*1. 顶部导航组件 */}
                 {/* children 组件标签之间的内容 */}
                  <NavHeader>地图找房</NavHeader>
